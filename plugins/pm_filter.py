d0f1fcc664e16202963e39540c99e1cd58cf38d9
SMP_TG <93422140+Smptech3938@users.noreply.github.com>
2022-01-19 17:23:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🎬[{get_size(file.file_size)}] 📂 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
