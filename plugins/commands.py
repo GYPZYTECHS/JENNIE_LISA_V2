bf46d6b1c0d02c77de2a80cf6f3a283abb1b0211
SMP_TG <93422140+Smptech3938@users.noreply.github.com>
2022-01-19 16:44:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -110,6 +110,11 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
+        buttons = [
+        [
+             InlineKeyboardButton('🛡 Join Group 🛡', url='t.me/MOVIESTALKIES23') 
+        ]
+        ]
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
