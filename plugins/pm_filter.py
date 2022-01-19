7c732b0919066aa04f3d7af2cd5538529ea5f0be
SMP_TG <93422140+Smptech3938@users.noreply.github.com>
2022-01-19 21:54:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -782,7 +782,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🎬[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🎬[{get_size(file.file_size)}] 📂 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
